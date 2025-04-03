0b137e17e316733ebf8644ea8196ea1a995bc510
Dave Pagurek <davepagurek@gmail.com>
2025-04-03 09:11:17 -0400

    Fix mixed indentation

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,15 +2,15 @@ import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 
 export default {
-	input: 'src/p5.woff2.js',
-	output: {
-		file: 'lib/p5.woff2.js',
-		format: 'umd',
+  input: 'src/p5.woff2.js',
+  output: {
+    file: 'lib/p5.woff2.js',
+    format: 'umd',
     name: 'woff2',
     globals: {
       p5: 'p5',
     },
-	},
+  },
   external: ['p5'],
   plugins: [commonjs({
     transformMixedEsModules: true,
